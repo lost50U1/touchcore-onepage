a6bfef442e9a9d06e0980df92c0b9b3c9fc64db9
Biruk Moges <birukmoges8@gmail.com>
2024-09-18 14:05:25 +0300

    intial-commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <main className="min-h-screen flex flex-col">
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </main>
+  );
+};
+
+export default App;
