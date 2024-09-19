753fa32c84f8adf74fbde631f97eff3189d8dc98
Biruk Moges <birukmoges8@gmail.com>
2024-09-19 19:44:38 +0300

    UPDATE Accessibility and More comments added

--- src/components/onelink/SearchDisplay.jsx
+++ src/components/onelink/SearchDisplay.jsx
@@ -1,5 +1,4 @@
 // import icons and images
-
 import {
   clockImg,
   flighticonImg,
@@ -8,7 +7,6 @@ import {
 } from "../../utils";
 
 // import mockdata as an api
-
 import {
   returnOptions,
   seatSelections,
@@ -17,7 +15,6 @@ import {
 } from "../../context/mockData";
 
 // import context api and data
-
 import { useBooking } from "../../context/useBooking";
 
 const SearchDisplay = () => {
@@ -27,9 +24,15 @@ const SearchDisplay = () => {
     <div className="bg-lightBlue py-6 pl-6 rounded-2xl flex flex-col">
       <div className="shadow-lg">
         <div className="bg-lightBlue py-1 pl-4 pr-2 shadow-lg flex flex-wrap w-full justify-between gap-2 text-xs min-h-[2.5625rem] font-bold">
+          {/* Return option select */}
+          <label htmlFor="returnSelect" className="sr-only">
+            Return
+          </label>
           <select
+            id="returnSelect"
             onChange={(e) => setSelectedReturn(e.target.value)}
             className="bg-lightBlue p-2"
+            aria-label="Return option"
           >
             {returnOptions.map((option, index) => (
               <option key={index} value={option}>
@@ -37,9 +40,16 @@ const SearchDisplay = () => {
               </option>
             ))}
           </select>
+
+          {/* Seat selection select */}
+          <label htmlFor="seatSelect" className="sr-only">
+            Seat Selection
+          </label>
           <select
+            id="seatSelect"
             onChange={(e) => setSelectedSeat(e.target.value)}
             className="bg-bgSecondary text-white px-2 py-1 rounded-sm"
+            aria-label="Seat selection"
           >
             {seatSelections.map((option, index) => (
               <option key={index} value={option}>
@@ -47,9 +57,16 @@ const SearchDisplay = () => {
               </option>
             ))}
           </select>
+
+          {/* Stops option select */}
+          <label htmlFor="stopsSelect" className="sr-only">
+            Stops
+          </label>
           <select
+            id="stopsSelect"
             onChange={(e) => setSelectedStop(e.target.value)}
             className="bg-white p-2"
+            aria-label="Stops option"
           >
             {stopsOptions.map((option, index) => (
               <option key={index} value={option}>
@@ -57,8 +74,13 @@ const SearchDisplay = () => {
               </option>
             ))}
           </select>
-          <button className="bg-white p-2">Airline</button>
+
+          {/* Airline button */}
+          <button className="bg-white p-2" aria-label="Select airline">
+            Airline
+          </button>
         </div>
+
         <div className="bg-white py-4 px-4 sm:px-[1.979375rem] flex justify-end gap-4">
           <div className="text-end">
             <p className="font-bold">{flights[0].departureTime}</p>
@@ -66,33 +88,33 @@ const SearchDisplay = () => {
           </div>
           <div className="flex flex-col justify-center items-center">
             <div className="flex items-center">
-              <div>
-                <img src={lefticonImg} alt="lefticon" />
-              </div>
+              {/* Left icon indicating departure */}
+              <img src={lefticonImg} alt="Left icon indicating departure" />
               <div className="flex items-center bg-lightestGray py-1 rounded-lg">
-                <div>
-                  <img src={flighticonImg} alt="flighticon" />
-                </div>
+                {/* Flight icon indicating flight details */}
+                <img
+                  src={flighticonImg}
+                  alt="Flight icon indicating flight details"
+                />
                 <p className="text-[0.625rem] text-bgSecondary">
                   {flights[0].stops}
                 </p>
               </div>
-              <div>
-                <img src={righticonImg} alt="righticon" />
-              </div>
+              {/* Right icon indicating arrival */}
+              <img src={righticonImg} alt="Right icon indicating arrival" />
             </div>
             <div className="flex items-center gap-1">
-              <div>
-                <img src={clockImg} alt="clock" />
-              </div>
+              {/* Clock icon indicating flight duration */}
+              <img src={clockImg} alt="Clock icon indicating flight duration" />
               <p className="text-xs">{flights[0].duration}</p>
             </div>
           </div>
         </div>
       </div>
-      <p className="font-bold text-base md:text-2xl mt-6 mb-4 text-start pr-6">
+
+      <h2 className="font-bold text-base md:text-2xl mt-6 mb-4 text-start pr-6">
         Search
-      </p>
+      </h2>
       <p className="text-darkGray text-start pr-6">
         Make searching flights an intuitive experience for your customers.
       </p>
